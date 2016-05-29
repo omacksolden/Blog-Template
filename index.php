1ef84c2c4e88dc2440f7295dd768379396d47aee
Olivia Mack-Solden <omacksolden@gmail.com>
2016-05-29 00:52:57 -0400

    Wordpress and PHP templates

--- index.php
+++ index.php
@@ -3,8 +3,17 @@
     <!-- Begin Section Container -->
          <section class="row">
               <div class="twelve columns">
-                  <h3>This is the Section</h3>
-                  <p>Section content goes here.</p>
+                  <!-- Begin Loop -->
+                  <?php
+                      if ( have_posts() ) {
+                          while ( have_posts() ) {
+                            the_post();
+                          } //end while
+                      } //end if
+
+
+                  ?>
+                  <!-- End Loop -->
               </div>
          </section>
      <!-- End Section Container -->
