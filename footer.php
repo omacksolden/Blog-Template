f851454183f285a4bd9d6a7d62c4f3d7718a58f8
Olivia Mack-Solden <omacksolden@gmail.com>
2016-05-28 23:20:28 -0400

    Header and footer separation

---
+++ footer.php
@@ -0,0 +1,9 @@
+        <footer class="row">
+             <div class="twelve columns">
+                 <h2>This is the Footer</h2>
+                 <p>Footer content goes here.</p>
+             </div>
+        </footer>
+      </div> <!-- End Container Div -->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Olivia Mack-Solden Blank Template</title>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+</head>
+<body>
+    <div class="container"> <!-- Begin Container Div -->
+        <header class="row">
+            <div class="twelve columns">
+                <h1>Olivia Mack-Solden Blank WordPress Template</h1>
+                <p>This is my WordPress template.</p>
+            </div>
+         </header>
