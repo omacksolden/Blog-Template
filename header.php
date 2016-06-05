cf3f980ddb4a1b158b052493bc05e0618d7de5cc
Olivia Mack-Solden <omacksolden@gmail.com>
2016-06-05 12:47:53 -0400

    End of blank template lectures

--- header.php
+++ header.php
@@ -11,7 +11,9 @@
     <div class="container"> <!-- Begin Container Div -->
         <header class="row">
             <div class="twelve columns">
-                <h1><?php bloginfo('name'); ?> </h1>
+                <h1><a href="<?php $url = home_url('/'); echo $url; ?>">
+                  <?php bloginfo('name'); ?>
+                </a></h1>
                 <h2><?php bloginfo('description'); ?></h2>
             </div>
          </header>
